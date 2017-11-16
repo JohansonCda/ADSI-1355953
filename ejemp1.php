4de7f9e3590ce73d0452dfbf34ef7cf0b1266424
Valeria Torres <valeria301199@hotmail.com>
2017-11-16 11:37:59 -0500

    Insert img

--- ejemp1.php
+++ ejemp1.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+	<meta charset="utf-8">
 	<title>EJEMPLO 1</title>
 </head>
 <body>
 		<h1>Hola beibis</h1>
+		<img src="https://lh4.ggpht.com/t-QH7NCzMn_YyGhzTyM21lQ_wstUH-OkiBbPCRV-_AwB5exnNCwXhnivJoipAzCqJh4=w300">
 </body>
 </html>
